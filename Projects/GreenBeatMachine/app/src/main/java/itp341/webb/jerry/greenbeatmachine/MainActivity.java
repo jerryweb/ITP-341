2b4cbe65928ffc945c75a144fc89b341529b8a9f
Jerry Webb <jerryweb@usc.edu>
2015-12-09 17:36:03 -0800

    Basic sound playback loaded

--- Projects/GreenBeatMachine/app/src/main/java/itp341/webb/jerry/greenbeatmachine/MainActivity.java
+++ Projects/GreenBeatMachine/app/src/main/java/itp341/webb/jerry/greenbeatmachine/MainActivity.java
@@ -1,21 +1,32 @@
 package itp341.webb.jerry.greenbeatmachine;
 
 import android.app.Activity;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends Activity {
     public static final String TAG = "itp341.webb.jerry.finalProject.tag";
     private MediaPlayer mp = new MediaPlayer();
+
+    //used to play sounds simultaneously... best used for sound files < 1MB
     private SoundPool samplePool;
+
+    int ben_k_id;
+    int phn_clp_id;
+    int dry_ohh_cra_id;
+    double masterVolume;
 
     Button btn_pad_0;
     Button btn_pad_1;
@@ -27,6 +38,8 @@ public class MainActivity extends Activity {
     Button btn_pad_7;
 
     ListView soundList;
+
+    SeekBar seekBarMasterVolume;
     ButtonListener buttonListener;
 
 
@@ -46,6 +59,42 @@ public class MainActivity extends Activity {
 
         soundList = (ListView) findViewById(R.id.soundsList);
         buttonListener = new ButtonListener();
+
+        btn_pad_0.setOnClickListener(buttonListener);
+        btn_pad_1.setOnClickListener(buttonListener);
+        btn_pad_2.setOnClickListener(buttonListener);
+        btn_pad_3.setOnClickListener(buttonListener);
+        btn_pad_4.setOnClickListener(buttonListener);
+        btn_pad_5.setOnClickListener(buttonListener);
+        btn_pad_6.setOnClickListener(buttonListener);
+        btn_pad_7.setOnClickListener(buttonListener);
+
+        //deprecated declaration
+        samplePool = new SoundPool(20, AudioManager.STREAM_MUSIC,0); //int maxStreams, int streamType,int srcQuality
+        ben_k_id = samplePool.load(this, R.raw.ben_k,1);
+        phn_clp_id = samplePool.load(this, R.raw.phn_clp,1);
+        dry_ohh_cra_id = samplePool.load(this, R.raw.dry_ohh_cra,1);
+
+        masterVolume =  0.8;
+        seekBarMasterVolume =  (SeekBar) findViewById(R.id.seekbarMasterVolume);
+
+        seekBarMasterVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                masterVolume = Double.parseDouble(String.valueOf(progress))/10;
+                Log.d(TAG,"" + masterVolume);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 
@@ -72,15 +121,22 @@ public class MainActivity extends Activity {
     }
 
     private class ButtonListener implements View.OnClickListener{
+//        float outputVolume = (float)masterVolume;
 
         @Override
         public void onClick(View v) {
+            Log.d(TAG,"outputVolume = "+ masterVolume);
+
             switch (v.getId()){
                 case R.id.btn_pad_0:
+                    samplePool.play(ben_k_id, (float) 0, (float) masterVolume, 1, 0, 1); //int soundID, float leftVolume,
+                    // float rightVolume,int priority,int loop,float rate
                     break;
                 case R.id.btn_pad_1:
+                    samplePool.play(phn_clp_id, (float)masterVolume,(float) masterVolume, 1, 0, 1); //int soundID, float leftVolume,
                     break;
                 case R.id.btn_pad_2:
+                    samplePool.play(dry_ohh_cra_id, (float)masterVolume, (float)masterVolume, 1, 0, 1); //int soundID, float leftVolume,
                     break;
                 case R.id.btn_pad_3:
                     break;
